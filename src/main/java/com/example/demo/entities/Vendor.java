793bfdc90f40b57742c04a77e86e2ee1b0482bb6
IdaFagerlund <ida.fagerlund@hotmail.com>
2019-11-25 12:31:57 +0100

    Add getter/setter on vendor. Add field serialVersionUID for vendor and user

--- src/main/java/com/example/demo/entities/Vendor.java
+++ src/main/java/com/example/demo/entities/Vendor.java
@@ -8,8 +8,19 @@ import javax.persistence.Id;
 @Entity
 public class Vendor {
 
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
+    public Vendor() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
