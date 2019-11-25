956764eaa154dfd2a65775d85ce75339ff173c4e
joeliths <your>
2019-11-25 13:44:28 +0100

    Create Customer- and PendingModel

---
+++ src/main/java/com/example/demo/models/CustomerModel.java
@@ -0,0 +1,20 @@
+package com.example.demo.models;
+
+import com.example.demo.entities.Customer;
+
+import java.io.Serializable;
+import java.util.Set;
+
+public class CustomerModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    //private UserModel user;
+    //private Set<PendingOrderModel> pendingOrders;
+
+
+
+
+
+
+}
