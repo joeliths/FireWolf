681efec008ff07f68deb7d67e86cc50bc6332ea8
IdaFagerlund <ida.fagerlund@hotmail.com>
2019-11-25 11:36:05 +0100

    Remove duplicate user model

--- src/main/java/com/example/demo/models/UserModel.java
+++ src/main/java/com/example/demo/models/UserModel.java
@@ -1,4 +1,32 @@
 package com.example.demo.models;
 
 public class UserModel {
+
+    private String fullName;
+    private String userName;
+
+    public UserModel() {
+    }
+
+    public UserModel(String fullName, String userName) {
+        this.fullName = fullName;
+        this.userName = userName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
 }
