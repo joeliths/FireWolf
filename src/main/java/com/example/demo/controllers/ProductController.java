ac6daa3d74297ab7f49a404451795b3e75488a86
Pontus <Pontus.fredriksson1@gmail.com>
2019-11-26 16:39:33 +0100

    add controller and service to product

---
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.demo.controllers;
+
+
+import com.example.demo.models.ProductModel;
+import com.example.demo.repositories.ProductRepository;
+import com.example.demo.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @PostMapping(path = "/products/add",
+            consumes = "application/json",
+            produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<?> addProductNewerWay(@RequestBody ProductModel productModel){
+        boolean answer = productService.addProduct(productModel);
+        return new ResponseEntity<>(answer, HttpStatus.OK);
+    }
+
+}
