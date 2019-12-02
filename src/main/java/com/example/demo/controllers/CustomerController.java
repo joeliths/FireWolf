b9fc4d493a824ecde83268258f763f86b2eba481
IdaFagerlund <ida.fagerlund@hotmail.com>
2019-12-02 17:15:31 +0100

    Create controller for pending order.

--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controllers;
 
-import com.example.demo.models.PendingOrderModel;
+
 import com.example.demo.services.PendingOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,43 +11,43 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-    @Autowired
-    PendingOrderService pendingOrderService;
-
-    @PostMapping(path = "/createPendingOrder",
-            consumes = "application/json",
-            produces = "application/json")
-    @ResponseBody
-    public ResponseEntity<?> addPendingOrder(@RequestBody PendingOrderModel pendingOrderModel){
-        pendingOrderService.addPendingOrder(pendingOrderModel);
-        return new ResponseEntity<>( HttpStatus.OK);
-    }
-//==================================================
-    @PostMapping(path = "/checkout",
-            consumes = "application/json",
-            produces = "application/json")
-    @ResponseBody
-    public ResponseEntity<?> checkoutPendingOrder(@RequestBody PendingOrderModel pendingOrderModel){
-        pendingOrderService.checkoutPendingOrder(pendingOrderModel);
-        return new ResponseEntity<>( HttpStatus.OK);
-    }
-
-    @PatchMapping(path = "/updateOrder",
-            consumes = "application/json",
-            produces = "application/json")
-    @ResponseBody
-    public ResponseEntity<?> updatePendingOrder(@RequestBody PendingOrderModel pendingOrderModel){
-        pendingOrderService.updatePendingOrder(pendingOrderModel);
-        return new ResponseEntity<>( HttpStatus.OK);
-    }
-
-    @DeleteMapping(path = "/deleteOrder",
-            consumes = "application/json",
-            produces = "application/json")
-    @ResponseBody
-    public ResponseEntity<?> deletePendingOrder(@RequestBody PendingOrderModel pendingOrderModel){
-        pendingOrderService.deletePendingOrder(pendingOrderModel);
-        return new ResponseEntity<>( HttpStatus.OK);
-    }
+//    @Autowired
+//    PendingOrderService pendingOrderService;
+//
+//    @PostMapping(path = "/createPendingOrder",
+//            consumes = "application/json",
+//            produces = "application/json")
+//    @ResponseBody
+//    public ResponseEntity<?> addPendingOrder(@RequestBody PendingOrderModel pendingOrderModel){
+//        pendingOrderService.addPendingOrder(pendingOrderModel);
+//        return new ResponseEntity<>( HttpStatus.OK);
+//    }
+////==================================================
+//    @PostMapping(path = "/checkout",
+//            consumes = "application/json",
+//            produces = "application/json")
+//    @ResponseBody
+//    public ResponseEntity<?> checkoutPendingOrder(@RequestBody PendingOrderModel pendingOrderModel){
+//        pendingOrderService.checkoutPendingOrder(pendingOrderModel);
+//        return new ResponseEntity<>( HttpStatus.OK);
+//    }
+//
+//    @PatchMapping(path = "/updateOrder",
+//            consumes = "application/json",
+//            produces = "application/json")
+//    @ResponseBody
+//    public ResponseEntity<?> updatePendingOrder(@RequestBody PendingOrderModel pendingOrderModel){
+//        pendingOrderService.updatePendingOrder(pendingOrderModel);
+//        return new ResponseEntity<>( HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping(path = "/deleteOrder",
+//            consumes = "application/json",
+//            produces = "application/json")
+//    @ResponseBody
+//    public ResponseEntity<?> deletePendingOrder(@RequestBody PendingOrderModel pendingOrderModel){
+//        pendingOrderService.deletePendingOrder(pendingOrderModel);
+//        return new ResponseEntity<>( HttpStatus.OK);
+//    }
 
 }
--- src/main/java/com/example/demo/services/PendingOrderService.java
+++ src/main/java/com/example/demo/services/PendingOrderService.java
@@ -1,41 +1,54 @@
 package com.example.demo.services;
 
-import com.example.demo.entities.PendingOrder;
-import com.example.demo.models.PendingOrderModel;
+import com.example.demo.entities.helperclasses.MyUUID;
+import com.example.demo.models.pendingorder.PendingOrderRequestModel;
+import com.example.demo.models.pendingorder.PendingOrderResponseModel;
 import com.example.demo.repositories.PendingOrderRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class PendingOrderService {
 
     final private PendingOrderRepository pendingOrderRepository;
+    final private ModelMapper modelMapper;
 
     @Autowired
-    private ModelMapper modelMapper;
-
-
-    @Autowired
-    public PendingOrderService(PendingOrderRepository pendingOrderRepository) {
+    public PendingOrderService(PendingOrderRepository pendingOrderRepository, ModelMapper modelMapper) {
         this.pendingOrderRepository = pendingOrderRepository;
+        this.modelMapper = modelMapper;
     }
 
-    public void addPendingOrder(PendingOrderModel pendingOrderModel){
-        PendingOrder pendingOrder = modelMapper.map(pendingOrderModel,PendingOrder.class);
-        pendingOrderRepository.saveAndFlush(pendingOrder);
+    public void addPendingOrder(PendingOrderRequestModel pendingOrder){
+    }
+
+    public List<PendingOrderResponseModel> getPendingOrdersForCustomer(String userName) {
+        return Collections.emptyList();
+    }
+
+    public List<PendingOrderResponseModel> getPendingOrdersForStore(MyUUID storeUUID) {
+        return Collections.emptyList();
+    }
+
+    public void getPendingOrders() {
 
     }
 
-    //Todo:write this method
-    public void checkoutPendingOrder(PendingOrderModel pendingOrderModel) {
+
+    public void checkoutPendingOrder(MyUUID pendingOrderUUID) {
     }
-    //Todo:write this method
-    public void updatePendingOrder(PendingOrderModel pendingOrderModel) {
+
+
+    public void deletePendingOrder(MyUUID pendingOrderUUID) {
     }
-    //TODO:write this method
-    public void deletePendingOrder(PendingOrderModel pendingOrderModel) {
+
+
+    public void updatePendingOrder(PendingOrderRequestModel pendingOrder) {
     }
+
+
 }
