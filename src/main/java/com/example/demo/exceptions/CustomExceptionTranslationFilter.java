f9658d6a92c69c0310c59331259bece2426c3da4
Daniella Noren <daniellanoren@live.se>
2020-01-07 10:42:14 +0100

    Remove unnecesssary

---
+++ src/main/java/com/example/demo/exceptions/CustomExceptionTranslationFilter.java
@@ -0,0 +1,32 @@
+package com.example.demo.exceptions;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomExceptionTranslationFilter extends ExceptionTranslationFilter {
+
+    public CustomExceptionTranslationFilter(AuthenticationEntryPoint authenticationEntryPoint) {
+        super(authenticationEntryPoint);
+    }
+
+    public CustomExceptionTranslationFilter(AuthenticationEntryPoint authenticationEntryPoint, RequestCache requestCache) {
+        super(authenticationEntryPoint, requestCache);
+    }
+
+    @Override
+    protected void sendStartAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, AuthenticationException reason) throws ServletException, IOException {
+        System.out.println("dundun");
+        System.out.println(reason.getClass().getSimpleName());
+        super.sendStartAuthentication(request, response, chain, reason);
+    }
+}
