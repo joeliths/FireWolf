6247793b053752018c332d3432460315ff33f59e
joeliths <your>
2020-01-10 13:10:58 +0100

    Change securityConfig so only vendor and admin can post new product

--- src/main/java/com/example/demo/security/securityConfig/SecurityConfiguration.java
+++ src/main/java/com/example/demo/security/securityConfig/SecurityConfiguration.java
@@ -62,6 +62,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.PATCH, "/products/**").hasRole("ADMIN")
                 .antMatchers("/vendor/**", "/store/**").hasRole("VENDOR")
                 .antMatchers(HttpMethod.GET, "/products/**").hasRole("VENDOR")
+                .antMatchers(HttpMethod.POST, "/products").hasAnyRole("VENDOR","ADMIN")
                 .antMatchers("/vendor", "/user", "/logout").hasRole("USER")
                 .antMatchers("/customer", "customer/**", "/pending-orders", "/pending-orders/**").hasRole("CUSTOMER")
                 .anyRequest().authenticated()
